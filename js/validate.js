8044cb2c19d98e3de5f3a5837662d3a04935664e
Oskar Plichta <oskar.plichta22@gmail.com>
2014-05-21 14:59:44 +0200

    Added validators for password

--- js/validate.js
+++ js/validate.js
@@ -6,14 +6,14 @@
 
 			return this.each(function (){
 				var isCorrect = false;
-				var val = $(this).val();
+				var value = $(this).val();
 				var patt;
 
 				if(options.pattern){
 					patt = new RegExp(options.pattern);
 					console.log("only pattern");
 					}
-					if(patt.test(val)){
+					if(patt.test(value)){
 						isCorrect = true;
 					}
 
@@ -30,12 +30,12 @@
 				var isCorrect = false;
 				var settings = $.extend({
 					"pattern": "[A-Za-z]\\w+"}, options);
-			return methods.regexp.call(this, settings)
+			return methods.regexp.call(this, settings);
+
 		},
 		emailExpression: function(options) {
 			//console.log("emailExpression: " + options);
 			var isCorrect = false;
-
 			var settings = $.extend({
 				"pattern": "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\." +
 				"[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@" +
@@ -43,7 +43,58 @@
 		        "[a-z0-9](?:[a-z0-9-]*[a-z0-9])?"}, options);
 
 			return methods.regexp.call(this, settings);
+		},
 
+		passwordExpression: function(options){
+
+      return this.each(function () {
+        var isCorrect = true;
+        var value = $(this).val();
+        var patt;
+        var min=5;
+        var max=10;
+
+        if(options.min){
+          if(value.length<min){
+            isCorrect = false;
+          }
+        }
+        if(options.max){
+          if(max<value.length){
+            isCorrect = false;
+          }
+        }
+        if(options.small){
+          patt = new RegExp("[a-z]");
+          if(!patt.test(value)){
+            isCorrect = false;
+          }
+        }
+        if(options.big){
+          patt = new RegExp("[A-Z]");
+          if(!patt.test(value)){
+            isCorrect = false;
+          }
+        }
+        if(options.dig){
+          patt = new RegExp("\\d");
+          if(!patt.test(value)){
+            isCorrect = false;
+          }
+        }
+        if(options.special){
+          patt = new RegExp("[\\!\\@\\#\\$\\%\\^\\&\\*\\(\\)\\_\\+\\-\\=]");
+          if(!pat.test(value)){
+            isCorrect = false;
+          }
+        }
+
+        if(isCorrect){
+          options.correct(this);
+        } else {
+          options.uncorrect(this);
+        }
+      });
 		}
 	};
 
--- js/validateScript.js
+++ js/validateScript.js
@@ -3,17 +3,18 @@ $('.btn').attr("disabled", true);
 function btnBlock(textErr,emailErr,passwordErr){
 	if(textErr+emailErr+passwordErr === 3){
 		$('.btn').attr("disabled", false);
-		$('.btn').addClass("btn-success");
+		$('.btn').removeClass("btn-block");
 		$('.btn').removeClass("btn-danger");
 	} else {
 		$('.btn').attr("disabled", true);
 		$('.btn').addClass("btn-danger");
-		$('.btn').removeClass("btn-success");
+		// $('.btn').removeClass("btn-success");
 	}
 }
 $('#textInput').keyup(function (){
-
 	$('#textInput').validate("textExpression",{
+		"min": true,
+		"max": true,
 		// "regex": "^[A-Z]\\w+",
 		"correct": function (that) {
 			textErr=1;
@@ -30,7 +31,6 @@ $('#textInput').keyup(function (){
 });
 
 $('#emailInput').keyup(function (){
-
 	$('#emailInput').validate("emailExpression",{
 				"correct": function (that) {
 					emailErr=1;
@@ -45,4 +45,24 @@ $('#emailInput').keyup(function (){
 		}
 	});
 
+});
+
+$('#passwordInput').keyup(function (){
+	$('#passwordInput').validate("passwordExpression",{
+		"small": true,
+		"big": true,
+		"dig": true,
+		"min": true,
+		"max": true,
+		"correct": function (that) {
+			passwordErr=1;
+			$(that).parent().removeClass("has-error");
+			btnBlock(textErr,emailErr,passwordErr);
+		},
+		"uncorrect": function (that){
+			passwordErr=0;
+			$(that).parent().addClass("has-error");
+			btnBlock(textErr,emailErr,passwordErr);
+		}
+	});
 });
